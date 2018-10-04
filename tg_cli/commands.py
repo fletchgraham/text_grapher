a2c8e59034f5ed9e7f25e5edf1bb4d710668d541
fletchgraham <sfletchgraham@gmail.com>
2018-10-03 22:56:13 -0400

    make a simple interface to actually save out an animation.

--- tg_cli/commands.py
+++ tg_cli/commands.py
@@ -1,8 +1,28 @@
 from fletch_tools.good_input import get_input
+from tg_utils.grapher import *
 
 def graph(args):
-    name = get_input('What is your name?')
-    print('Hello {}.'.format(name))
+
+    char = get_input('Enter a character to represent empty space.')
+    draw_char = get_input('Enter a character with which to draw the object.')
+    dst = get_input('Enter a name for the folder of text files.')
+
+    # build a blank grid
+    my_blank = BlankGraph(size=44, char=char)
+    my_graph = my_blank.build()
+
+    # create a camera
+    my_camera = Camera(location=[0, 0, -16], rotation=[0, 0, 0])
+
+    # create cube
+    my_cube = Mesh(location=[0,0,0], rotation=[pi/8, pi/4, pi/16], scale=[2, 2, 2])
+
+    my_camera.draw(my_graph, my_cube, draw_char)
+
+    joined = "".join(my_graph)
+    print(joined)
+
+    rotate_animation(pi/64, 2*pi, dst)
 
 def wh_help(args=''):
     if args:
