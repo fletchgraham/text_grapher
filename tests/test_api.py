40197ff034dd23255c5836da22b5e1ebedc36714
fletchgraham <sfletchgraham@gmail.com>
2020-01-22 00:54:00 -0500

    tests for getters of mesh object

--- tests/test_api.py
+++ tests/test_api.py
@@ -3,3 +3,16 @@ import text_grapher as tg
 def test_scene_init():
     scene = tg.Scene()
     assert scene.frame_start == 1
+    assert scene.camera.location == (0,0,10)
+
+def test_mesh_init():
+    mesh = tg.Mesh()
+
+    assert mesh.location == (0, 0, 0)
+    assert mesh.rotation == (0, 0, 0)
+    assert mesh.scale_factor == (1, 1, 1)
+
+    assert mesh.character == 'M'
+
+    assert mesh.vertices == []
+    assert mesh.edges == []
