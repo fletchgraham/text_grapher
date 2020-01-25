4954f6abdf923337721fcfe0a6436fbb70a6e185
fletchgraham <sfletchgraham@gmail.com>
2020-01-24 23:57:36 -0500

    simplify setup, this breaks the example though.

--- setup.py
+++ setup.py
@@ -2,13 +2,5 @@ from setuptools import setup
 setup(
     name = 'text_grapher',
     version = '0.1.0',
-    packages = [
-        'tg_cli',
-        'tg_utils',
-        'fletch_tools'
-        ],
-    entry_points = {
-        'console_scripts': [
-            'tg = tg_cli.main:main'
-        ]
-    })
+    packages = ['text_grapher']
+    )
