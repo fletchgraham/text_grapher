cb4f3003cfead823a97ff4cf60c2de15798a05bd
fletchgraham <sfletchgraham@gmail.com>
2020-01-24 20:37:25 -0500

    move vector tests to test_maths

--- tests/test_maths.py
+++ tests/test_maths.py
@@ -1,4 +1,20 @@
-from text_grapher.maths import matrix_product
+from text_grapher.maths import (
+    matrix_product,
+    vector_to_matrix,
+    matrix_to_vector,
+    Vector
+    )
+
+def test_vector_init():
+    vector = Vector()
+    assert vector.x == 0
+    assert vector.y == 0
+    assert vector.z == 0
+
+def test_add_vectors():
+    v1 = Vector(1, 3, 4)
+    v2 = Vector(2, 4, -10)
+    assert Vector.add(v1, v2) == Vector(3, 7, -6)
 
 def test_matrix_product():
     A = [[0, 1],
