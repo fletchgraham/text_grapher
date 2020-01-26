72f0327824d26efab14c6217110a7041a6505d3c
fletchgraham <sfletchgraham@gmail.com>
2020-01-26 12:27:24 -0500

    make the firework example into a looping animation

--- tg_examples/fireworks.py
+++ tg_examples/fireworks.py
@@ -13,9 +13,9 @@ class Firework:
         ".'`,^:;~"
         ]
 
-    def __init__(self, start, graph):
-        self.x = randint(0, 39)
-        self.y = randint(0, 39)
+    def __init__(self, start, x, y, graph):
+        self.x = x
+        self.y = y
         self.start = start
         self.graph = graph
 
@@ -23,7 +23,7 @@ class Firework:
         r = (frame - self.start) * .5
         if r > 27:
             return
-        for t in range(800):
+        for t in range(100):
             x = r * cos(t) + self.x
             y = r * sin(t) + self.y
 
@@ -34,20 +34,35 @@ class Firework:
 if __name__ == '__main__':
 
     scene = tg.Scene()
-    scene.name = 'explosions_example'
+    scene.name = 'fireworks_example'
     scene.graph.background = ' '
 
+    # make a list of fireworks to draw
     fireworks = []
+    for frame in range(200):
+        if not frame % 10:
+            f = Firework(
+                frame,
+                randint(0,39),
+                randint(0,39),
+                scene.graph
+                )
+            fireworks.append(f)
+
+    # copy fireworks and prepend so the animation loops
+    prior_fireworks = [
+        Firework(f.start - 200, f.x, f.y, f.graph)
+        for f in fireworks
+        ]
+
+    fireworks = prior_fireworks + fireworks
 
     @scene.animate
     def fireworks_show(frame):
-        if not frame % 10:
-            f = Firework(frame, scene.graph)
-            fireworks.append(f)
-
         for firework in fireworks:
-            firework.draw(frame)
+            if firework.start <= frame:
+                firework.draw(frame)
 
-    scene.frame_stop = 500
+    scene.frame_stop = 200
     scene.render(open_player=True)
-    # scene.render_gif()
+    # scene.render_gif(invert=True)
