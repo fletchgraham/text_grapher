8e84f578edaf2a114a10cf5a56708feed9a60808
fletchgraham <sfletchgraham@gmail.com>
2020-01-27 20:33:24 -0500

    write matrix transformations and tests

--- tests/test_maths.py
+++ tests/test_maths.py
@@ -1,9 +1,5 @@
-from text_grapher.maths import (
-    matrix_product,
-    vector_to_matrix,
-    matrix_to_vector,
-    Vector
-    )
+import pytest
+from text_grapher.maths import *
 
 def test_vector_init():
     vector = Vector()
@@ -39,3 +35,37 @@ def test_matrix_vector_conversions():
     assert m == [[2], [3], [4], [1]]
     v2 = matrix_to_vector(m)
     assert v2 == v
+
+# Transformations:
+
+def test_rotate_vector():
+    v1 = Vector(1, 0, 0)
+    R = rotation_matrix(z = 3.141/2)
+    v2 = Vector.transform(v1, R)
+
+    print(v2)
+    assert v2.y == pytest.approx(-1)
+
+def test_translate_vector():
+    v1 = Vector(0, 1, 0)
+    T = translation_matrix(5, 3, 5)
+    v2 = Vector.transform(v1, T)
+    assert v2 == (5, 4, 5)
+
+def test_scale_vector():
+    v1 = Vector(2, 2, 0)
+    S = scale_matrix(3, 4, 0)
+    v2 = Vector.transform(v1, S)
+    assert v2 == (6, 8, 0)
+
+def test_multiple_transforms():
+    v1 = Vector(1, 0, 0)
+    S = scale_matrix(3, 3, 3)
+    R = rotation_matrix(y = 3.14159)
+    T = translation_matrix(y=-1)
+    M = matrix_product(R, S)
+    M = matrix_product(T, M)
+    v2 = Vector.transform(v1, M)
+    assert v2.y == pytest.approx(-1)
+    assert v2.x == pytest.approx(-3)
+    assert v2.z == pytest.approx(0, abs=1e-3) # set wider tolerance
--- text_grapher/maths.py
+++ text_grapher/maths.py
@@ -1,6 +1,18 @@
 """Math concepts like vectors and matrices"""
 
+from math import sin, cos
 from collections import namedtuple
+
+def identity_matrix():
+    """Return an identity matrix
+
+    This is a function instead of a global variable because you want to know
+    it hasn't been altered."""
+
+    return [[1, 0, 0, 0],
+            [0, 1, 0, 0],
+            [0, 0, 1, 0],
+            [0, 0, 0, 1]]
 
 class Vector(namedtuple('Vector', ['x', 'y', 'z'], defaults=[0,0,0])):
 
@@ -12,24 +24,92 @@ class Vector(namedtuple('Vector', ['x', 'y', 'z'], defaults=[0,0,0])):
             v1.z + v2.z
             )
 
+    @classmethod
+    def transform(cls, vector, matrix):
+        """Return a new vector with the transformation matrix applied"""
+        P = vector_to_matrix(vector)
+        M = matrix
+        MP = matrix_product(M, P)
+        new_vector = matrix_to_vector(MP)
+        return new_vector
+
 def matrix_product(A, B):
-
+    """Return the matrix product: AB"""
     C = [[0 for i in A] for j in B]
-
     for i in range(len(A)):
-
         for j in range(len(B[0])):
-
             for k in range(len(B)):
-
                 C[i][j] += A[i][k] * B[k][j]
-
     return C
 
 def vector_to_matrix(vector):
+    """Return an iterable like a Vector as a matrix"""
     M = [[i] for i in vector]
     M.append([1])
     return M
 
 def matrix_to_vector(matrix):
-    return tuple([i[0] for i in matrix[:-1]])
+    """Return a Tuple from a matrix... this should return a Vector"""
+    if len(matrix) == 4:
+        x = matrix[0][0]
+        y = matrix[1][0]
+        z = matrix[2][0]
+        return Vector(x, y, z)
+    else:
+        return tuple([i[0] for i in matrix[:-1]])
+
+def rotation_matrix(x=0, y=0, z=0):
+    """Return a rotation matrix for the given rotation.
+
+    The parameters result in clockwise rotation in radians around each axis.
+    """
+    m = identity_matrix()
+
+    rot_x = [[1, 0, 0, 0],
+             [0, cos(x), sin(x), 0],
+             [0, -sin(x), cos(x), 0],
+             [0, 0, 0, 1]]
+
+    rot_y = [[cos(y), 0, -sin(y), 0],
+             [0, 1, 0, 0],
+             [sin(y), 0, cos(y), 0],
+             [0, 0, 0, 1]]
+
+    rot_z = [[+cos(z), +sin(z), 0, 0],
+             [-sin(z), +cos(z), 0, 0],
+             [0, 0, 1, 0],
+             [0, 0, 0, 1]]
+
+    p = matrix_product(rot_x, m)
+    p = matrix_product(rot_y, p)
+    p = matrix_product(rot_z, p)
+
+    return p
+
+def translation_matrix(x=0, y=0, z=0):
+    """Return a translation matrix for the given translation values."""
+    m = identity_matrix()
+
+    translate_m = [[1, 0, 0, x],
+                 [0, 1, 0, y],
+                 [0, 0, 1, z],
+                 [0, 0, 0, 1]]
+
+    p = matrix_product(translate_m, m)
+    return p
+
+def scale_matrix(x=0, y=0, z=0):
+    """Return a scale matrix for the given scale factor."""
+
+    m = identity_matrix()
+    scale_m = [[x, 0, 0, 0],
+            [0, y, 0, 0],
+            [0, 0, z, 0],
+            [0, 0, 0, 1]]
+
+    p = matrix_product(scale_m, m)
+    return p
+
+def transform_verts(verts_list, matrix):
+    """Return a new list of verts with the matrix applied"""
+    return [Vector.transform(vert) for vert in verts_list]
